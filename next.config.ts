bb36f0b502c351b7a956f575e7431e0936c905cb
fevico <ajayifavour81@gmail.com>
2025-08-28 12:05:45 +0100

    fix static path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  basePath: '/admin',
+  assetPrefix: '/admin/',
   images: {
     domains:   ['res.cloudinary.com'] ,
 
